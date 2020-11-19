6e3d9a27580cf3b8544d51fbf22559ad69086e54
Manish Sharma <manish.sharma0398@gmail.com>
2020-11-19 10:35:04 +0530

    adding header component and lines to home, shop page and contact

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import { Route, Switch } from "react-router-dom";
 import HomePage from "./pages/homepage/HomePage";
 import "./App.css";
 import ShopPage from "./pages/shop/Shop";
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
