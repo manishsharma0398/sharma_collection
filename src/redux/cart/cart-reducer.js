287eb34de58b40e35126904636e3b73942e2c32d
Manish Sharma <manish.sharma0398@gmail.com>
2020-11-23 09:45:20 +0530

    updated shop data with new data, updated cart utils to group items in cart reducer

--- src/redux/cart/cart-reducer.js
+++ src/redux/cart/cart-reducer.js
@@ -1,13 +1,21 @@
 import { cartTypes } from "./cart-types";
+import { addItemToCart } from "./cart-utils";
 
 const initialState = {
   hidden: true,
+  cartItems: [],
 };
 
-const cartReducer = (state = initialState, { type }) => {
+const cartReducer = (state = initialState, { type, payload }) => {
   switch (type) {
     case cartTypes.TOGGLE_CART_HIDDEN:
       return { ...state, hidden: !state.hidden };
+
+    case cartTypes.ADD_ITEM:
+      return {
+        ...state,
+        cartItems: addItemToCart(state.cartItems, payload),
+      };
 
     default:
       return state;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,16 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import logger from "redux-logger";
 
 import rootReducer from "./root-reducer";
 
 const middleware = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middleware));
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
 
 export default store;
